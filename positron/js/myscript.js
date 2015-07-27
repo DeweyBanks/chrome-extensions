e11bd5ccd4c414cc959b2ff715f5eb043efb11c7
Dewey Banks <DeweyBanks@gmail.com>
2015-07-27 14:19:22 -0400

    adds work

--- positron/js/myscript.js
+++ positron/js/myscript.js
@@ -1,9 +1,15 @@
 window.addEventListener('mouseup',function(event)
 {
 	var sel = window.getSelection().toString();
+  // console.log($(‘.a3s’).first().html());
+  var txt = '';
+  $('.a3s p').each(function(){
+    txt = txt + $(this).text();
+  });
 
+    console.log(txt);
 	if(sel.length)
 		chrome.extension.sendRequest({'message':'setText','data': sel},function(response){
-      alert(response);
+      $('.a3s p').attr('style', 'color:red;');
     })
 })
